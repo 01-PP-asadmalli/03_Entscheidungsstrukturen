a07fcae2072ade5d71f4fb060acdb23b1325b51c
asadmalli <asadmaqsoodmalli@gmail.com>
2023-05-10 12:11:56 +0200

    Fallunterscheidung / SWITCH|CASE 1

--- script_01.js
+++ script_01.js
@@ -3,22 +3,23 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
-const ageJohn = 20;
-const ageMark = 30;
+// const ageJohn = 20;
+// const ageMark = 30;
 
-// Deklaration
-let isJohnOlder, isJohnEqual;
+// // Deklaration
+// let isJohnOlder, isJohnEqual;
 
-// Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// // Test | Logische Aussage
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
-// Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
+
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -32,6 +33,7 @@ console.log("------------------");
 // {
 //     console.log ("John ist älter.");
 // }
+
 
 /************ IF - ELSE  ************/
 // mit Alternative  
@@ -52,19 +54,59 @@ console.log("------------------");
 /************  Ternäre (ternary) Schreibweise ************/
 // console.log( (isJohnOlder) ? "John is older" : "Mark is older");
 
+
 /************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
-if (isJohnOlder)  // 1.Test
+// if (isJohnOlder)  // 1.Test
+// {
+//   console.log ("John ist älter.");
+// } 
+// else if (isJohnEqual)  // 1......n Alternative
+// {
+//   console.log ("John ist gleich alt.");
+// } 
+// // wenn alle vorherigen Test fehlschlagen.....
+// else  
+// {
+//   console.log ("John ist jünger.");  
+// }
+
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job;
+
+job = "driver";                                         // ...fährt Taxi/UBER
+// job = "diver";                                          // .. taucht im Rhein!
+// job = "arist";                                          // .. malt ein Bild!
+// job = "Polit";                                          // .. macht etwas anders! -->default
+// job = "teacher";                                        // .. unterrichtet!
+// job = "instructor";                                     // .. unterrichtet!
+
+
+switch (job) 
 {
-  console.log ("John ist älter.");
-} 
-else if (isJohnEqual)  // 1......n Alternative
-{
-  console.log ("John ist gleich alt.");
-} 
-// wenn alle vorherigen Test fehlschlagen.....
-else  
-{
-  console.log ("John ist jünger.");  
+    case "driver":                                      // ( == )
+        console.log (firstName,"fährt Taxi");
+        break;
+    case "diver":                                      
+        console.log (firstName,"taucht im Rhein!");
+        break;
+    case "arist":                                      
+        console.log (firstName,"malt ein Bild!");
+        break;
+    case "teacher": 
+    case "instructor":                                     
+        console.log (firstName,"unterrichtet!");
+        break;
+
+    default:
+        console.log (firstName,"macht etwas anders!");
+        break;
 }
+
+
+
+
